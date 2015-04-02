84d0965d8473dc2e6ae02ab3374513684d7bd1f1
Cesar B Rocha <crocha@ucsd.edu>
2015-04-01 18:07:03 -0700

    minor changes; plotting labels and title

--- src/dp_map.py
+++ src/dp_map.py
@@ -1,8 +1,9 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 
 class drake_passage_map(object):
-
+    """ a class that represents the Drake Passage Basemap object """
     
     def __init__(
         self,
@@ -14,8 +15,9 @@ class drake_passage_map(object):
         lon_0=-75,
         proj="lcc",
         linewidth=.5,
-        fontsize=25.
-        ):
+        fontsize=18.,
+        fig_label="c",
+        fig_title="AlTiKa"):
 
             self.lonmin = lonmin
             self.lonmax = lonmax
@@ -26,7 +28,9 @@ class drake_passage_map(object):
             self.proj = proj
             self.lw = linewidth
             self.fs = fontsize
-            
+            self.label= fig_label
+            self.title = fig_title
+
             self.m = Basemap(llcrnrlon=self.lonmin,llcrnrlat=self.latmin,\
                     urcrnrlon=self.lonmax, urcrnrlat=self.latmax,
                     rsphere=(6378137.00,6356752.3142),
@@ -34,7 +38,19 @@ class drake_passage_map(object):
                     lat_1=self.lat_1,lon_0=self.lon_0)
 
 
-    def draw_par_mer():
+    def draw_par_mer(self):
+        """ draw parallel and meridians """
+        self.m.drawparallels(np.arange(self.latmin, self.latmax, 4),
+            labels=[1, 0, 0, 0], linewidth=self.lw, fontsize=self.fs)
+        self.m.drawmeridians(np.arange(self.lonmin, self.lonmax+4, 7),
+            labels=[0, 0, 0, 1], linewidth=self.lw, fontsize=self.fs)
+
+    def set_label(self,pos=(1650212,1485371)):
+        plt.text(pos[0],pos[1], self.label,size=32)
+
+    def set_title(self,pos=(1400212,1495371)):
+        plt.text(pos[0],pos[1], self.title, size=25, rotation=0.,
+            ha="center", va="center",bbox = dict(boxstyle="round",ec='k',fc='w'))
 
 
 
